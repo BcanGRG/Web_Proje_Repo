a951f7c5c0078d47416454bbb9eb84c5ad33841e
Burak Can <57682748+BcanGRG@users.noreply.github.com>
2020-12-23 02:14:36 +0300

    Add project files.

---
+++ WebFood/Models/Category/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebFood.Models.Category
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+    }
+}
